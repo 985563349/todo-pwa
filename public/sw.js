d453779a384d4bdd68573934b645a44ed2cd850e
王杰 <jie985563349@outlook.com>
2023-02-23 11:44:11 +0800

    add cache

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,19 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open('todo-pwa-cache').then((cache) => cache.addAll(['/index.html'])));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== 'todo-pwa-cache')
+            .map((cacheName) => caches.delete(cacheName))
+        )
+      )
+  );
+});
+
 self.addEventListener('fetch', (event) => {});
